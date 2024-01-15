c6e353e4e9ae3d7232e7e11dbf621cfcbfbcc103
OrzeFox <dacm1997@yahoo.com>
2024-01-15 17:31:08 +0100

    funciones parte 1 done

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -1,23 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcruz <dcruz@student.42barcelona.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/11 10:29:06 by dcruz             #+#    #+#             */
-/*   Updated: 2024/01/15 16:50:05 by dcruz            ###   ########.fr       */
+/*   Created: 2024/01/15 16:40:08 by dcruz             #+#    #+#             */
+/*   Updated: 2024/01/15 17:01:40 by dcruz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlen(const char *s)
+char *ft_strjoin(char const *s1, char const *s2)
 {
-    size_t i = 0;
-    while(s[i] != '\0')
+    size_t a = 0;
+    size_t b = 0;   
+    char *concat = malloc(ft_strlen(s1) + ft_strlen(s2) + 1); 
+    if (!s1 || !s2 || !concat) 
+        return NULL;
+    while (s1[a] != '\0') 
     {
-        i++;
+        concat[a] = s1[a]; 
+        a++;
     }
-    return (i);
+    while (s2[b] != '\0') 
+    {
+        concat[a] = s2[b]; 
+        a++;
+        b++;
+    }
+    concat[a] = '\0';
+
+    return concat;
 }
